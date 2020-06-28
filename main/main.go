44a0c5e4d84e8692c023758a53a662bf5266186d
Gregório Patriota <gpc.cin@gmail.com>
2020-06-28 20:34:08 -0300

    Using map

--- main/main.go
+++ main/main.go
@@ -7,22 +7,36 @@ import (
 )
 
 func main() {
-	x := make([]int, 1, 1)
-	x[0] = 10
-	fmt.Println(x)
-	fmt.Println("Length is", len(x))
-	fmt.Println("Capacity is", cap(x))
-	x = append(x, 20, 30, 40, 50)
-	fmt.Println(x)
-	fmt.Println("Length is", len(x))
-	fmt.Println("Capacity is", cap(x))
-	fmt.Println(x)
-	x = append(x, 60)
-	fmt.Println("Length is", len(x))
-	fmt.Println("Capacity is", cap(x))
-	fmt.Println(x)
-	x = append(x, 60, 70, 80, 90, 100)
-	fmt.Println("Length is", len(x))
-	fmt.Println("Capacity is", cap(x))
-	fmt.Println(x)
+	// x := make([]int, 1, 1)
+	// x[0] = 10
+	// fmt.Println(x)
+	// fmt.Println("Length is", len(x))
+	// fmt.Println("Capacity is", cap(x))
+	// x = append(x, 20, 30, 40, 50)
+	// fmt.Println(x)
+	// fmt.Println("Length is", len(x))
+	// fmt.Println("Capacity is", cap(x))
+	// fmt.Println(x)
+	// x = append(x, 60)
+	// fmt.Println("Length is", len(x))
+	// fmt.Println("Capacity is", cap(x))
+	// fmt.Println(x)
+	// x = append(x, 60, 70, 80, 90, 100)
+	// fmt.Println("Length is", len(x))
+	// fmt.Println("Capacity is", cap(x))
+	// fmt.Println(x)
+
+	dict := make(map[string]string)
+	dict["go"] = "Golang"
+	dict["cs"] = "CSharp"
+	dict["rb"] = "Ruby"
+	dict["py"] = "Python"
+	dict["js"] = "JavaScript"
+	for k, v := range dict {
+		fmt.Printf("Key: %s Value: %s\n", k, v)
+	}
+
+	if lan, ok := dict["ts"]; ok {
+		fmt.Println(lan, ok)
+	}
 }
