fbe059006343f3d4f70e8d83bfddc8b0beac75ec
Gregório Patriota <gpc.cin@gmail.com>
2020-06-28 20:20:59 -0300

    Slices in Go

--- main/main.go
+++ main/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	calc "chapter1/calc"
-	strcon "chapter2/strcon"
+	_ "chapter1/calc"
+	_ "chapter2/strcon"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, world")
-	fmt.Println(calc.Add(1, 2))
-	s := strcon.SwapCase("Gopher")
-	fmt.Println("Converted string is :", s)
+	x := make([]int, 1, 1)
+	x[0] = 10
+	fmt.Println(x)
+	fmt.Println("Length is", len(x))
+	fmt.Println("Capacity is", cap(x))
+	x = append(x, 20, 30, 40, 50)
+	fmt.Println(x)
+	fmt.Println("Length is", len(x))
+	fmt.Println("Capacity is", cap(x))
+	fmt.Println(x)
+	x = append(x, 60)
+	fmt.Println("Length is", len(x))
+	fmt.Println("Capacity is", cap(x))
+	fmt.Println(x)
+	x = append(x, 60, 70, 80, 90, 100)
+	fmt.Println("Length is", len(x))
+	fmt.Println("Capacity is", cap(x))
+	fmt.Println(x)
 }
--- src/chapter2/strcon/strcon.go
+++ src/chapter2/strcon/strcon.go
@@ -8,8 +8,8 @@ import (
 func SwapCase(str string) string {
 	buf := &bytes.Buffer{}
 	for _, r := range str {
-	if unicode.IsUpper(r) {
-		buf.WriteRune(unicode.ToLower(r))
+		if unicode.IsUpper(r) {
+			buf.WriteRune(unicode.ToLower(r))
 		} else {
 			buf.WriteRune(unicode.ToUpper(r))
 		}
